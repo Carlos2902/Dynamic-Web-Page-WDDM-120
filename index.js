af9cbc22fc9464d946036f2776aeaa025f1138e9
Carlos Lopez <carloslopezr29@gmail.com>
2024-01-29 19:19:14 -0500

    JavaScript added for the changes in the main content

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.getElementsByClassName('starbucks')[0].src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.getElementsByClassName('circle')[0];
+    circle.style.background = color;
+}
